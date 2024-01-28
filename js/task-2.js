63e107f4d7369d300ef069d76b1df806f3b95e80
antagonist13 <1703shakayaroslaw@gmail.com>
2024-01-28 16:11:19 +0200

    task 2

--- js/task-2.js
+++ js/task-2.js
@@ -8,14 +8,18 @@
 // Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
 
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+    return `${message}`
+    }
+    else {
+        return `${message.slice(0, maxLength) + "..."}`
+    }
+}
 
-
-
-// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-// Залиш цей код для перевірки ментором.
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
